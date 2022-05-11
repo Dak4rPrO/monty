1f8d351141e2f45462981db96a18b59b72e9a07c
Dak4rPrO <shinoflow111@gmail.com>
2022-05-10 19:09:57 -0700

    betty a readfile y monty

--- readfile.c
+++ readfile.c
@@ -1,38 +1,37 @@
 #include "monty.h"
 
 /**
- * @brief 
- * 
+ * main - monty
+ * @argc: argument count
+ * @argv: argument variable
+ * Return: return 0
  */
 int main(int argc __attribute__((unused)), char *argv[])
 {
 	const char *filename = argv[1];
-    	size_t bufsize = 0;
+	size_t bufsize = 0;
 	char *buff = NULL;
-	FILE* _open = fopen(filename, "r");
+	FILE *_open = fopen(filename, "r");
 
-	/*char *buff = malloc(sizeof(char) * letters);
+	buff = malloc(sizeof(char));
 
 	if (buff == NULL)
 	{
 		printf("Error: malloc failed\n");
-		exit (EXIT_FAILURE);
+		exit(EXIT_FAILURE);
 	}
-*/	
-    
-	/*if (_open == NULL)
+
+	if (_open == NULL)
 	{
 		printf("Error: Can't open file %s\n", filename);
 		free(buff);
-		exit (EXIT_FAILURE);
-	}*/
+		exit(EXIT_FAILURE);
+	}
 
 	while (getline(&buff, &bufsize, _open) != -1)
-	{
 		printf("%s", buff);
-	}
-	
-	/*free(buff);*/
+
+	free(buff);
 
 	return (0);
 }
