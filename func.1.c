2565e98746cc336f7f2b3a39a2115e984812a881
Dak4rPrO <shinoflow111@gmail.com>
2022-05-12 11:03:54 -0700

    todo

--- func.1.c
+++ func.1.c
@@ -23,3 +23,20 @@ void push(stack_t **stack, __attribute__ ((unused))unsigned int line_number)
 		(*stack)->prev = top;
 	*stack = top;
 }
+
+/**
+ * _pall - print all function
+ * @stack: pointer to linked list stack
+ * @line_number: number of line opcode occurs on
+ */
+void pall(stack_t **stack, __attribute__ ((unused))unsigned int line_number)
+{
+	stack_t *dentrostack;
+
+	dentrostack = *stack;
+	while (dentrostack != NULL)
+	{
+		printf("%d\n", dentrostack->n);
+		dentrostack = dentrostack->next;
+	}
+}
--- monty.h
+++ monty.h
@@ -41,5 +41,6 @@ extern int push_arg;
 void push(stack_t **stack, __attribute__ ((unused))unsigned int line_number);
 int main(int argc __attribute__((unused)), char *argv[]);
 void (*get_func(char *opcode))(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, __attribute__ ((unused))unsigned int line_number);
 
 #endif
