79b775f49e2ba4d15e2cc864bb77a2a0e42564e0
Dak4rPrO <shinoflow111@gmail.com>
2022-05-12 23:20:47 -0700

    edit prototipo del main.h

--- monty.h
+++ monty.h
@@ -40,7 +40,7 @@ typedef struct instruction_s
 extern int push_arg;
 void Free_s(stack_t **stack);
 void push(stack_t **stack, unsigned int line_number);
-int main(int argc __attribute__((unused)), char *argv[]);
+int main(int argc, char *argv[]);
 void (*get_func(char *opcode))(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, __attribute__ ((unused))unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
